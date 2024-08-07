a688e4164e378bc5cb3b139fe47d2268d409d189
sakibhossain323 <sakibhossain323@gmail.com>
2024-08-07 12:40:11 +0600

    add spring boot backend

---
+++ kanbento-backend/src/main/java/com/canseesharp/kanbento/KanbentoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.canseesharp.kanbento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbentoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbentoBackendApplication.class, args);
+	}
+
+}
---
+++ kanbento-backend/src/test/java/com/canseesharp/kanbento/KanbentoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canseesharp.kanbento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbentoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
