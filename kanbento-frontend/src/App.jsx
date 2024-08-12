eb9ef51078707bf7f05ece1e3127f1f3464c05aa
sakibhossain323 <sakibhossain323@gmail.com>
2024-08-12 18:04:23 +0600

    add react router and bootstrap

--- kanbento-frontend/src/App.jsx
+++ kanbento-frontend/src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1 className="text-success">home</h1>,
+    },
+    {
+        path: "/about",
+        element: <h1>about</h1>,
+    },
+]);
 
 function App() {
-    return <>Kanbento Frontend</>;
+    return <RouterProvider router={router} />;
 }
 
 export default App;
